433f2d825831371f7e3ddadf8c84325092d567dc
Indhu Bandi <indhubandi@Indhus-MacBook-Air.local>
2026-04-07 18:42:57 +0530

    Developed PasteDetection and added new feature

--- src/components/PasteStats.tsx
+++ src/components/PasteStats.tsx
@@ -1,5 +1,3 @@
-
-
 type Props = {
   wordCount: number;
   charCount: number;
@@ -21,7 +19,18 @@ export default function PasteStats({
       <h3>Characters: {charCount}</h3>
       <h3>Paste Count: {pasteCount}</h3>
       <h3>Last Pasted Length: {pastedLength}</h3>
-      <p>{warning}</p>
+
+      {warning && (
+        <p
+          style={{
+            color: "red",
+            fontWeight: "bold",
+            marginTop: "10px",
+          }}
+        >
+          ⚠️ {warning}
+        </p>
+      )}
     </div>
   );
 }
